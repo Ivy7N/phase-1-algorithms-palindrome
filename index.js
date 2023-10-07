1643864c23e469859fe4e98ae14705e658fed5d0
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-10-07 14:16:44 +0300

    complete algorithm

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const lowercaseWord = word.toLowerCase();
 
 /* 
   Add your pseudocode here
 */
-
+const cleanedWord = lowercaseWord.replace(/[^a-z]/g, '');
 /*
   Add written explanation of your solution here
 */
+const reversedWord = cleanedWord.split('').reverse().join('');
+  return cleanedWord === reversedWord;
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
